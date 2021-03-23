61ba4149e7b30c323f20485344db1eab8d2e7ca9
Piotr Pokornowski <ppokornowski.ti118@zsp1piotrkow.edu.pl>
2021-03-23 15:55:11 +0100

    updated html, added js

---
+++ script.js
@@ -0,0 +1,27 @@
+function addPerson() {
+    console.log("addPerson");
+}
+
+function showTable() {
+    console.log("showTable");
+}
+
+function deleteSpecific() {
+    console.log("deleteSpecific");
+}
+
+function deleteAll() {
+    console.log("deleteAll");
+}
+
+function fontSizeIncrease() {
+    console.log("fontSizeIncrease");
+}
+
+function fontSizeDecrease() {
+    console.log("fontSizeDecrease");
+}
+
+function themeChange() {
+    console.log("themeChange");
+}

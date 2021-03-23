c0c089783c98a12cd52068c6be2b214e8c944c60
Piotr Pokornowski <ppokornowski.ti118@zsp1piotrkow.edu.pl>
2021-03-23 17:08:09 +0100

    js

--- script.js
+++ script.js
@@ -1,15 +1,58 @@
-var firstName
+var firstName;
+var surname;
+var age;
+var id = 1;
+var people = [];
+let tab = document.getElementById("tab");
 
 
 function addPerson() {
-    console.log("addPerson");
+    firstName = document.getElementById("firstName").value;
+    surname = document.getElementById("surname").value;
+    age = parseInt(document.getElementById("age").value);
+    let person = {
+        id,
+        firstName,
+        surname,
+        age
+    };
+    people.push(person);
+    id = id + 1;
 }
 
 function showTable() {
-    console.log("showTable");
+    let person = {
+
+    }
+    for(let i = 0; i < people.length; i++)
+    {
+        person = {
+            person_id: people[i].id,
+            person_firstName: people[i].firstName,
+            person_surname: people[i].surname,
+            person_age: people[i].age
+        }
+     /*   document.getElementById("tab").innerHTML += 
+        `<tr>
+            <td>${person_id}</td>
+            <td>${person_firstName}</td>
+            <td>${person_surname}</td>
+            <td>${person_age}</td>
+        </tr>`/*/
+        let row = tab.insertRow(i)
+        let cellId = row.insertCell(0);
+        let cellFirstName = row.insertCell(1);
+        let cellSurname = row.insertCell(2);
+        let cellAge = row.insertCell(3);
+        cellId.innerText = person.person_id;
+        cellFirstName.innerText = person.person_firstName;
+        cellSurname.innerText = person.person_surname;
+        cellAge.innerText = person.person_age;
+    }
 }
 
 function deleteSpecific() {
+    rowId = 
     console.log("deleteSpecific");
 }
 
